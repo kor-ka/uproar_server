d35c9d107c2102e5ec4114bf0e018de0f45d1bd8
gputintsev <g.putintsev@qiwi.ru>
2017-03-14 20:28:53 +0300

    print on device online tracks

--- ChatActor.py
+++ ChatActor.py
@@ -402,7 +402,9 @@ class ChatActor(pykka.ThreadingActor):
 
     def on_device_online(self, token, device):
         for t in self.latest_tracks.get():
+            pprint(vars(t))
             try:
+                print "time dif:" + time() - t.time < 60 * 15 + " time:" + time() + " t.time:" + t.time
                 if time() - t.time < 60 * 15:
                     status = t.device_status.get(token.split('-')[1])
                     if status is None or status.startswith(downloading) or status.startswith(
