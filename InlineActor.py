653c67539f8936093610b94879ae2535d9b31b00
gputintsev <g.putintsev@qiwi.ru>
2017-07-11 13:45:39 +0300

    feat(inline): add inline search

--- InlineActor.py
+++ InlineActor.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 import pykka
 from splinter import Browser
+import urllib
 
 
 class InlineActor(pykka.ThreadingActor):
@@ -13,8 +14,6 @@ class InlineActor(pykka.ThreadingActor):
         super(InlineActor, self).__init__()
         self.bot = bot
         self.browser = None
-        self.count = 0
-        self.q_time = datetime.now()
 
     def on_start(self):
         try:
@@ -22,14 +21,14 @@ class InlineActor(pykka.ThreadingActor):
             suffix = ".apt/usr/bin/google-chrome-stable"
             chrome_path = os.getenv("GOOGLE_CHROME_SHIM", "")
             prefix = chrome_path[:-len(suffix)]
-            os.environ['PATH'] = os.getenv("PATH", "")  + ":" + prefix + ".chromedriver/bin:" + chrome_path
+            os.environ['PATH'] = os.getenv("PATH", "") + ":" + prefix + ".chromedriver/bin:" + chrome_path
             executable_path = {'executable_path': "/app/.chromedriver/bin/chromedriver"}
 
             #  executable_path = {'executable_path': '/tmp/build_3eb58544f5f97e761b0afd5314624668/kor-ka-uproar_server-bcbb420/.chromedriver/bin/chromedriver'}
 
             print executable_path
-            
-           #self.browser = Browser('chrome', **executable_path)
+
+            # self.browser = Browser('chrome', **executable_path)
             self.browser = Browser('phantomjs')
             self.browser.driver.set_window_size(640, 480)
 
@@ -46,32 +45,17 @@ class InlineActor(pykka.ThreadingActor):
             print "Inline Actor msg" + str(message)
             if message.get('command') == 'q':
                 self.on_query(message.get('q'))
-                # if message.get('time') is None:
-                #     self.q_debounce(message.get('q'))
-                # elif self.q_time == message.get('time'):
-                #     self.on_query(message.get('q'))
 
         except Exception as ex:
             logging.exception(ex)
 
-    def q_debounce(self, query):
-        delayed = PostDelayed()
-        self.q_time = delayed.time
-        delayed.post(self.actor_ref, {'time': delayed.time, 'q':query, 'command':'q'}, 800)
-
     def on_query(self, query):
         if len(query.query) > 0:
             res = []
-            self.browser.visit('http://m.vk.com/audio?act=search&q=' + query.query)
 
+            print ('start search: ' + query.query)
+            self.browser.visit('http://m.vk.com/audio?act=search&q=' + urllib.urlencode(query.query))
 
-            # self.browser.find_by_id('au_search_field').first.fill(query.query)
-
-            # driver = self.browser.driver
-            # wait = WebDriverWait(driver, 10)
-            # wait.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, '.al_loading .qs_loading')))
-
-            # print self.browser.html.encode('ascii', 'xmlcharrefreplace')
             for body in self.browser.find_by_css(".ai_body"):
 
                 try:
@@ -93,19 +77,3 @@ class AudioResult(object):
     def __init__(self, url, title):
         self.url = url
         self.title = title
-
-class PostDelayed(threading.Thread):
-    def __init__(self):
-        self.running = True
-        threading.Thread.__init__(self)
-        self.time = datetime.now()
-        super(PostDelayed, self).__init__()
-
-    def run(self):
-        while self.running:
-            pass
-
-    def post(self, ref, message, delay):
-        sleep(delay)
-        ref.tell(message)
-        self.running = False
