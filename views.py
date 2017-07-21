725111717eb9b1f4fd8a6f25c2674922b028009a
gputintsev <g.putintsev@qiwi.ru>
2017-07-21 15:56:41 +0300

    feat(flask):move payment to form

--- views.py
+++ views.py
@@ -15,7 +15,7 @@ def run(manager):
 
     @app.route('/pay_callback', methods=['POST'])
     def pay_callback():
-        manager.tell({"command":"payment", "data":request.data})
+        manager.tell({"command":"payment", "data":request.form})
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 
