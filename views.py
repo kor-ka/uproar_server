7df3139fc57d7c4e89d52b8ec8f52dd97efcb1fe
gputintsev <g.putintsev@qiwi.ru>
2017-07-21 14:50:52 +0300

    feat(flask):move payment to form

--- views.py
+++ views.py
@@ -10,7 +10,7 @@ def run(manager):
     @app.route('/')
     @app.route('/crown')
     def index():
-        return '<iframe src="https://money.yandex.ru/quickpay/button-widget?targets=Crown%F0%9F%91%91&default-sum=10&button-text=12&any-card-payment-type=on&button-size=l&button-color=orange&successURL=http%3A%2F%2Ft.me%2Fuproarbot%3Fstart%3Ddone&quickpay=small&account=410011799344776&" width="227" height="48" frameborder="0" allowtransparency="true" scrolling="no"></iframe>'
+        return '<iframe src="https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=Crown%F0%9F%91%91&targets-hint=&default-sum=10&button-text=12&payment-type-choice=on&hint=&successURL=http%3A%2F%2Ft.me%2Fuproarbot%3Fstart%3Ddone&quickpay=shop&account=410011799344776" width="450" height="204" frameborder="0" allowtransparency="true" scrolling="no"></iframe>'
 
     @app.route('/pay_callback')
     def pay_callback():
