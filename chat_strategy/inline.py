aab177a806e333060861694bb60e98097d243286
kor-ka <badgrib@gmail.com>
2018-02-06 23:36:34 +0300

    feat(inline): save tracks

--- chat_strategy/inline.py
+++ chat_strategy/inline.py
@@ -12,7 +12,7 @@ def on_query(q, chat_actor):
 
     liked_tracks_db = chat_actor.context.storage.ask(
         {'command': 'get_list', 'name': Storage.LIKED_TRACKS_TABLE, "suffix": q.from_user.id}) # type: DbList
-    liked_tracks = liked_tracks_db.get(limit=10, order="DESC", offset=0 if q.offset is None else q.offset)
+    liked_tracks = liked_tracks_db.get(limit=10, order="DESC", offset=0 if q.offset is None or len(str(q.offset)) == 0 else q.offset)
     res = []
     for track in liked_tracks:
         res.append(InlineQueryResultAudio(str(q.from_user.id)  + "_"+ str(track.get("file_id")), chat_actor.get_d_url(track.get("file_id")), track.get("title"), performer=track.get("artist"), audio_duration=track.get("duration")))
